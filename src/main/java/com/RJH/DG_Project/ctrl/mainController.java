5ac7fea39ce0962a0e381c8f8a9e7a4ce483baeb
Ketchup <vkd5744@naver.com>
2024-01-30 17:00:36 +0900

    mainController edit

--- src/main/java/com/RJH/DG_Project/ctrl/mainController.java
+++ src/main/java/com/RJH/DG_Project/ctrl/mainController.java
@@ -1,13 +1,19 @@
 package com.RJH.DG_Project.ctrl;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class mainController {
 
     @GetMapping("/api/hello")
     public String test() {
         return "Hello, world!";
     }
+
+    /*@RequestMapping("/")
+    public String main() {
+        return "index";
+    }*/
 }
